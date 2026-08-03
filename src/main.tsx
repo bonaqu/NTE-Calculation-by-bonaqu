b537d253e59f192c28fb034a026c926e69ea6dd7
ᴇᴜɢᴇɴᴇ <48132049+bonaqu@users.noreply.github.com>
2026-08-03 17:39:06 +0300

    style: load Arc database styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,5 +4,6 @@ import App from './App';
 import { I18nProvider } from './i18n';
 import './styles.css';
 import './arc-scenarios.css';
+import './database.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><I18nProvider><App /></I18nProvider></StrictMode>);
